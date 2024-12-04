7d5c018d916450ffbfaf4555c09b20b3360baae6
Sina-Hosseini-GST <sina.hosseini.gst@gmail.com>
2024-12-05 01:10:04 +0330

    Migrate from CSS transitions to GSAP animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "./globals.css";
 import { useState, useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 export default function RootLayout({
   children,
@@ -9,9 +10,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
+  const duration = .5;
 
   const menuRef = useRef<HTMLDivElement | null>(null);
+  const menuItemNo1Ref = useRef<HTMLAnchorElement | null>(null);
+  const menuItemNo2Ref = useRef<HTMLAnchorElement | null>(null);
+  const menuItemNo3Ref = useRef<HTMLAnchorElement | null>(null);
   const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const spanNo1Ref = useRef<HTMLSpanElement | null>(null);
+  const spanNo2Ref = useRef<HTMLSpanElement | null>(null);
+  const spanNo3Ref = useRef<HTMLSpanElement | null>(null);
+  const bulletNo1Ref = useRef<HTMLSpanElement | null>(null);
+  const bulletNo2Ref = useRef<HTMLSpanElement | null>(null);
+  const bulletNo3Ref = useRef<HTMLSpanElement | null>(null);
+  const iconNo1Ref = useRef<SVGSVGElement | null>(null);
+  const iconNo2Ref = useRef<SVGSVGElement | null>(null);
+  const iconNo3Ref = useRef<SVGSVGElement | null>(null);
 
   const [isClicked, setIsClicked] = useState(false);
 
@@ -30,12 +44,219 @@ export default function RootLayout({
   };
 
   useEffect(() => {
+    if (isClicked) {
+      gsap.to(buttonRef.current, {
+        gap: '3px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(spanNo1Ref.current, {
+        rotate: '45deg',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(spanNo2Ref.current, {
+        height: '0px',
+        duration: duration / 2,
+        ease: 'power4.out',
+      });
+
+      gsap.to(spanNo3Ref.current, {
+        rotate: '-45deg',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuRef.current, {
+        left: '50%',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(bulletNo1Ref.current, {
+        delay: 0,
+        opacity: 1,
+        translateX: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(bulletNo2Ref.current, {
+        delay: duration / 4,
+        opacity: 1,
+        translateX: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(bulletNo3Ref.current, {
+        delay: duration / 2,
+        opacity: 1,
+        translateX: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuItemNo1Ref.current, {
+        delay: 0,
+        opacity: 1,
+        translateX: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuItemNo2Ref.current, {
+        delay: duration / 4,
+        opacity: 1,
+        translateX: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuItemNo3Ref.current, {
+        delay: duration / 2,
+        opacity: 1,
+        translateX: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(iconNo1Ref.current, {
+        delay: .5,
+        opacity: 1,
+        translateY: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(iconNo2Ref.current, {
+        delay: .625,
+        opacity: 1,
+        translateY: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(iconNo3Ref.current, {
+        delay: .75,
+        opacity: 1,
+        translateY: '0px',
+        duration,
+        ease: 'power4.out',
+      });
+    }
+    else {
+      gsap.to(buttonRef.current, {
+        gap: '0.25rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(spanNo1Ref.current, {
+        rotate: '0deg',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(spanNo2Ref.current, {
+        height: '3px',
+        duration: duration / 2,
+        ease: 'power4.out',
+      });
+
+      gsap.to(spanNo3Ref.current, {
+        rotate: '0deg',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuRef.current, {
+        left: 'calc(100% + 2.75rem)',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(bulletNo1Ref.current, {
+        delay: 0,
+        opacity: 0,
+        translateX: '-1rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(bulletNo2Ref.current, {
+        delay: duration / 4,
+        opacity: 0,
+        translateX: '-1rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(bulletNo3Ref.current, {
+        delay: duration / 2,
+        opacity: 0,
+        translateX: '-1rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuItemNo1Ref.current, {
+        delay: 0,
+        opacity: 0,
+        translateX: '2rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuItemNo2Ref.current, {
+        delay: duration / 4,
+        opacity: 0,
+        translateX: '2rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(menuItemNo3Ref.current, {
+        delay: duration / 2,
+        opacity: 0,
+        translateX: '2rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(iconNo1Ref.current, {
+        delay: .5,
+        opacity: 0,
+        translateY: '1.5rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(iconNo2Ref.current, {
+        delay: .625,
+        opacity: 0,
+        translateY: '1.5rem',
+        duration,
+        ease: 'power4.out',
+      });
+
+      gsap.to(iconNo3Ref.current, {
+        delay: .75,
+        opacity: 0,
+        translateY: '1.5rem',
+        duration,
+        ease: 'power4.out',
+      });
+    }
+
     document.addEventListener('click', hideMenu);
 
     return () => {
       document.removeEventListener('click', hideMenu);
     };
-  }, []);
+  }, [isClicked]);
 
 
   return (
@@ -45,34 +266,34 @@ export default function RootLayout({
       >
         <header className='fixed inset-x-0 top-0 p-6'>
           <nav className="flex justify-end">
-            <button className={`size-11 bg-[#f9ec0c] rounded-md z-20 flex flex-col ${isClicked ? 'gap-[3px]' : 'gap-1'} justify-center items-center`} onClick={toggleMenu} ref={buttonRef}>
-              <span className={`bg-black rounded-full duration-500 transition-transform w-3 h-[3px] ${isClicked && 'rotate-45'}`}></span>
-              <span className={`bg-black rounded-full duration-[250ms] transition-[height] w-3 ${isClicked ? 'h-0' : 'h-[3px]'}`}></span>
-              <span className={`bg-black rounded-full duration-500 transition-transform w-3 h-[3px] ${isClicked && '-rotate-45'}`}></span>
+            <button className={`size-11 bg-[#f9ec0c] rounded-md z-20 flex flex-col gap-[3px] justify-center items-center`} onClick={toggleMenu} ref={buttonRef}>
+              <span className={`bg-black rounded-full w-3 h-[3px]`} ref={spanNo1Ref}></span>
+              <span className={`bg-black rounded-full w-3 h-[3px]`} ref={spanNo2Ref}></span>
+              <span className={`bg-black rounded-full w-3 h-[3px]`} ref={spanNo3Ref}></span>
             </button>
-            <div className={`${isClicked ? 'left-1/2' : 'left-[calc(100%+2.75rem)]'} z-10 absolute w-1/2 inset-y-0 bg-[#008C44] top-0 h-svh transition-[left] duration-500 flex flex-col justify-center items-center`} ref={menuRef}>
+            <div className={`z-10 absolute w-1/2 inset-y-0 bg-[#008C44] top-0 h-svh flex flex-col justify-center items-center left-[calc(100%+2.75rem)]`} ref={menuRef}>
               <div className='text-8xl uppercase'>
                 <div className='flex items-center gap-8'>
-                  <span className={`text-lg text-white delay-[0ms] transition-[opacity,transform] duration-500 ${isClicked ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-4'}`}>
+                  <span className={`text-lg text-white opacity-0 -translate-x-4`} ref={bulletNo1Ref}>
                     I
                   </span>
-                  <a href={'/foodchin'} className={`py-4 text-[#f9ec0c] hover:text-white transition-[color,transform] delay-[0ms,0ms] duration-500 ${isClicked ? 'translate-x-0' : 'translate-x-8'}`}>
+                  <a href={'/foodchin'} className={`py-4 text-[#f9ec0c] hover:text-white transition-colors duration-500 translate-x-8 opacity-0`} ref={menuItemNo1Ref}>
                     foodchin
                   </a>
                 </div>
                 <div className='flex items-center gap-8'>
-                  <span className={`text-lg text-white delay-[250ms] transition-[opacity,transform] duration-500 ${isClicked ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-4'}`}>
+                  <span className={`text-lg text-white opacity-0 -translate-x-4`} ref={bulletNo2Ref}>
                     II
                   </span>
-                  <a href={'/about'} className={`py-4 text-[#f9ec0c] hover:text-white transition-[color,transform] delay-[0ms,250ms] duration-500 ${isClicked ? 'translate-x-0' : 'translate-x-8'}`}>
+                  <a href={'/about'} className={`py-4 text-[#f9ec0c] hover:text-white transition-colors duration-500 translate-x-8 opacity-0`} ref={menuItemNo2Ref}>
                     about
                   </a>
                 </div>
                 <div className='flex items-center gap-8'>
-                  <span className={`text-lg text-white delay-[375ms] transition-[opacity,transform] duration-500 ${isClicked ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-4'}`}>
+                  <span className={`text-lg text-white opacity-0 -translate-x-4`} ref={bulletNo3Ref}>
                     III
                   </span>
-                  <a href={'/contact'} className={`py-4 text-[#f9ec0c] hover:text-white transition-[color,transform] delay-[0ms,375ms] duration-500 ${isClicked ? 'translate-x-0' : 'translate-x-8'}`}>
+                  <a href={'/contact'} className={`py-4 text-[#f9ec0c] hover:text-white transition-colors duration-500 translate-x-8 opacity-0`} ref={menuItemNo3Ref}> 
                     contact
                   </a>
                 </div>
@@ -80,17 +301,17 @@ export default function RootLayout({
               <ul className='absolute flex gap-4 bottom-8 inset-x-0 justify-center'>
                 <li>
                   <a href="#">
-                    <svg className={`size-6 transition-[opacity,transform] duration-500 delay-[875ms] stroke-[#f9ec0c] fill-transparent ${isClicked ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg>
+                    <svg ref={iconNo1Ref} className={`size-6 stroke-[#f9ec0c] opacity-0 translate-y-6 fill-transparent`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg>
                   </a>
                 </li>
                 <li>
                   <a href="#">
-                    <svg className={`size-6 transition-[opacity,transform] duration-500 delay-[1000ms] stroke-[#f9ec0c] fill-transparent ${isClicked ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
+                    <svg ref={iconNo2Ref} className={`size-6 stroke-[#f9ec0c] opacity-0 translate-y-6 fill-transparent`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
                   </a>
                 </li>
                 <li>
                   <a href="#">
-                    <svg className={`size-6 transition-[opacity,transform] duration-500 delay-[1125ms] stroke-[#f9ec0c] fill-transparent ${isClicked ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+                    <svg ref={iconNo3Ref} className={`size-6 stroke-[#f9ec0c] opacity-0 translate-y-6 fill-transparent`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect width="4" height="12" x="2" y="9"></rect><circle cx="4" cy="4" r="2"></circle></svg>
                   </a>
                 </li>
               </ul>
