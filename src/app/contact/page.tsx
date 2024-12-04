926bc0f49473cc369a584608d85c901cc7683bfc
Sina-Hosseini-GST <sina.hosseini.gst@gmail.com>
2024-12-04 23:22:30 +0330

    Complete the first three tasks

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <section className='bg-black text-white flex justify-center items-center h-svh'>
+      contact
+    </section>
+  )
+}
+
+export default Page
