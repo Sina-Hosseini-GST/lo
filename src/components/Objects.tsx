ff12387b2640caa14d58b0559cfd564dc856f583
Sina-Hosseini-GST <sina.hosseini.gst@gmail.com>
2024-12-07 13:38:54 +0330

    Replace random images with a SVG

--- src/components/Objects.tsx
+++ src/components/Objects.tsx
@@ -39,23 +39,23 @@ const Objects: React.FC<NumberOfObjects> = ({ numberOfObjects }) => {
 
     const boxes: Matter.Body[] = [];
 
-    const imageSize = 300;
-
     for (let i = 0; i < numberOfObjects; i++) {
-      const size = 120 + (Math.random() * 30);
-      // Simulate delay by "(i * size)"
-      const x = windowWidth + (i * size);
+      const boxSize = 100 + (Math.random() * 25);
+      // Simulate delay by "(i * boxSize)"
+      const x = windowWidth + (i * boxSize);
       const y = (windowHeight * .75 * Math.random());
-      const imageUrl = `https://picsum.photos/${imageSize}/${imageSize}?random=${i}`;
+      const texture = `https://s2.svgbox.net/materialui.svg?ic=fastfood`;
+      // Using the link above, the texture has the width and height of 24
+      const textureSize = 24;
       const speed = 80 + (Math.random() * 20);
 
-      const box = Matter.Bodies.rectangle(x, y, size, size, {
+      const box = Matter.Bodies.rectangle(x, y, boxSize, boxSize, {
         restitution: .25,
         render: {
           sprite: {
-            texture: imageUrl,
-            xScale: size / imageSize,
-            yScale: size / imageSize,
+            texture,
+            xScale: boxSize / textureSize,
+            yScale: boxSize / textureSize,
           },
         },
       });
